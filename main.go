d6ddcdb5c68a8badd3a9790f44e6688d177ed770
Samet Yigitkalan <sametykalan@gmail.com>
2024-10-10 15:34:07 +0300

    feat: remove other needed flags

--- main.go
+++ main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"myditor/terminal"
+	"os"
+)
+
+func main(){
+    fd := int(os.Stdin.Fd())
+    original := terminal.EnableRaw(fd)
+    terminal.LoopInput()
+    defer terminal.DisableRaw(fd, original)
+}
--- terminal/terminal.go
+++ terminal/terminal.go
@@ -1,16 +1,22 @@
 package terminal
 
 import (
+	"fmt"
 	"os"
+	"unicode"
+
 	"golang.org/x/sys/unix"
 )
-
 
 func EnableRaw(fd int) *unix.Termios {
 	termios := GetTermios(fd)
 
 	oldState := termios
-	termios.Lflag &^= unix.ECHO
+    // Look manpage of termios(3) for more information about these flags
+	termios.Lflag &^= unix.ECHO | unix.ICANON | unix.ISIG | unix.IEXTEN 
+    termios.Iflag &^= unix.IXON | unix.ICRNL | unix.BRKINT | unix.INPCK | unix.ISTRIP
+    termios.Oflag &^= unix.OPOST
+    termios.Cflag &^= unix.CS8
 
 	unix.IoctlSetTermios(fd, unix.TCSETS, termios)
 	return oldState
@@ -19,7 +25,6 @@ func EnableRaw(fd int) *unix.Termios {
 func DisableRaw(fd int, original *unix.Termios) {
 	unix.IoctlSetTermios(fd, unix.TCSETS, original)
 }
-
 
 func GetTermios(fd int) *unix.Termios {
 	term, err := unix.IoctlGetTermios(fd, unix.TCGETS)
@@ -35,6 +40,11 @@ func LoopInput() {
 		if err != nil {
 			break
 		}
+		if unicode.IsControl(rune(b)) {
+            fmt.Printf("%d\n\r", b)
+        } else{
+            fmt.Printf("%d ('%c')\n\r", b, b)
+        }
 		if b == 'q' {
 			break
 		}
